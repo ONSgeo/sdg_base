d289f86f2b8c929c314ab1c5e557328d22095cec
EdCussONS <148250246+EdCussONS@users.noreply.github.com>
2024-01-03 15:40:07 +0000

    add files by upload

---
+++ src/sdg_base_src/__init__.py
@@ -0,0 +1,10 @@
+"""SDGs"""
+# import os, sys 
+# sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+
+from .sdg_base import SDGBase
+from typing import List
+
+__all__: List[str] = [
+    "SDGBase", 
+]
---
+++ tests/sdg_base_tests.py
@@ -0,0 +1,66 @@
+from typing import Optional
+import unittest
+from unittest.mock import patch
+
+import pandas as pd
+import geopandas as gpd
+
+from src.sdg_base_src import SDGBase
+from ..user_params import UserParams
+
+
+params: UserParams = UserParams()
+
+
+class SDGConcrete(SDGBase):
+    def __init__(self, sdg_name: str, root_dir: Optional[str], data_dir: Optional[str] = None, output_dir: Optional[str] = None, logger: bool = False) -> None:
+        super().__init__(sdg_name, root_dir, data_dir, output_dir, logger)
+
+    def calculate_sdg(self) -> bool:
+        return True
+
+
+class TestSDGBase(unittest.TestCase):
+    
+    @classmethod
+    def setUpClass(cls) -> None:
+        print('Setting up class')
+    
+
+    def setUp(self) -> None:
+        self._instance1: SDGConcrete = SDGConcrete('', params.root_dir)
+        
+
+    def test_get_input_data_dir(self) -> None:
+        print('running test_get_input_data_dir')
+        self.assertIsNotNone(self._instance1.get_input_data_dir())
+        self.assertIsInstance(self._instance1.get_input_data_dir(), str)
+    
+
+    def test_set_input_data_dir(self) -> None:
+        print('running test_set_input_data_dir')
+        self._instance1.set_input_data_dir(params.root_dir)
+        self.assertEqual(self._instance1.get_input_data_dir(), params.root_dir)
+
+
+    def test_get_output_data_dir(self) -> None:
+        print('running test_get_output_data_dir')
+        self.assertIsNotNone(self._instance1.get_output_data_dir())
+        self.assertIsInstance(self._instance1.get_output_data_dir(), str)
+
+
+    def test_set_output_data_dir(self) -> None:
+        print('running test_set_output_data_dir')
+        self._instance1.set_output_data_dir(params.root_dir)
+        self.assertEqual(self._instance1.get_output_data_dir(), params.root_dir)
+
+
+    def test_get_read_function(self) -> None:
+        print('running test_get_read_function')
+        self.assertIs(self._instance1._get_read_function('csv'), pd.read_csv)
+        self.assertIs(self._instance1._get_read_function('xlsx'), pd.read_excel)
+        self.assertIs(self._instance1._get_read_function('shp'), gpd.read_file)
+        
+        
+
+        
